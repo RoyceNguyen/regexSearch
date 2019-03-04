1a6dffe2236c71a68a46f52784ba38c27e2877da
Royce Nguyen <Hoangtu.Nguyen11@stclairconnect.ca>
2019-03-03 23:02:09 -0500

    list all files in directory and filter through all the txt files

--- regexSearch.py
+++ regexSearch.py
@@ -1,3 +1,17 @@
 #Search for a regex pattern in all txt files in a directory.
 
 import os, re
+
+# List all files in directory and filter those that aren't txt files
+files = os.listdir(os.getcwd())
+txtFiles = []
+
+txtRegex = re.compile(r'.txt')
+
+for doc in files:
+    if txtRegex.search(doc) is not None:
+        txtFiles.append(doc)
+
+
+
+#open each txt file and if regex triggers print to console
